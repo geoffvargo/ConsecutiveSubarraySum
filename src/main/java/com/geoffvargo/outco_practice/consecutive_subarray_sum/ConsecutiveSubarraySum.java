c67363a92c128af8ad4897b696e000595fe723b9
geoffvargo <geoff.vargo@stonybrook.edu>
2022-01-23 18:20:53 -0500

    INITIAL COMMIT

---
+++ src/main/java/com/geoffvargo/outco_practice/consecutive_subarray_sum/ConsecutiveSubarraySum.java
@@ -0,0 +1,24 @@
+package com.geoffvargo.outco_practice.consecutive_subarray_sum;
+
+/**
+ * Given an array of positive integers and a target value, return true if there is a subarray of consecutive elements that sum up to this target value.
+ * <pre>
+ *      Input: Array of integers, target value
+ *      Output: Boolean
+ * </pre>
+ *   <h3>Example:</h3>
+ * <pre>
+ *      Input: [6,12,1,7,5,2,3], 14     =>    Output: true (7+5+2)
+ *      Input: [8,3,7,9,10,1,13], 50    =>    Output: false
+ * </pre>
+ *   <h3>Constraints:</h3>
+ * <pre>
+ *      Time Complexity: O(N)
+ *      Auxiliary Space Complexity: O(1)
+ *      All elements are positive
+ * </pre>
+ */
+public class ConsecutiveSubarraySum {
+	public static void main(String[] args) {
+	}
+}
