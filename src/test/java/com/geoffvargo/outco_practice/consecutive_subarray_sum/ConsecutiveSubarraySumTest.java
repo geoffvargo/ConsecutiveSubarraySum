70bfe05b080650e63fea897a550f16934cdb5171
geoffvargo <geoff.vargo@stonybrook.edu>
2022-01-23 20:35:11 -0500

    unit-test class added (update 1)

--- src/test/java/com/geoffvargo/outco_practice/consecutive_subarray_sum/ConsecutiveSubarraySumTest.java
+++ src/test/java/com/geoffvargo/outco_practice/consecutive_subarray_sum/ConsecutiveSubarraySumTest.java
@@ -13,29 +13,8 @@ class ConsecutiveSubarraySumTest {
 	
 	@org.junit.jupiter.api.Test
 	void consecSubarraySum_TEST2() {
-		int   target = 14;
-		int[] arr    = {6, 12, 1, 7, 5, 2, 3};
-		assertEquals(true, ConsecutiveSubarraySum.consecSubarraySum(arr, target));
-	}
-	
-	@org.junit.jupiter.api.Test
-	void consecSubarraySum_TEST3() {
-		int   target = 14;
-		int[] arr    = {6, 12, 1, 7, 5, 2, 3};
-		assertEquals(true, ConsecutiveSubarraySum.consecSubarraySum(arr, target));
-	}
-	
-	@org.junit.jupiter.api.Test
-	void consecSubarraySum_TEST4() {
-		int   target = 14;
-		int[] arr    = {6, 12, 1, 7, 5, 2, 3};
-		assertEquals(true, ConsecutiveSubarraySum.consecSubarraySum(arr, target));
-	}
-	
-	@org.junit.jupiter.api.Test
-	void consecSubarraySum_TEST5() {
-		int   target = 14;
-		int[] arr    = {6, 12, 1, 7, 5, 2, 3};
-		assertEquals(true, ConsecutiveSubarraySum.consecSubarraySum(arr, target));
+		int   target = 50;
+		int[] arr    = {8, 3, 7, 9, 10, 1, 13};
+		assertEquals(false, ConsecutiveSubarraySum.consecSubarraySum(arr, target));
 	}
 }
