ac21bc42f2e678903470eb839d247e86ea1a626c
geoffvargo <geoff.vargo@stonybrook.edu>
2022-01-23 21:30:43 -0500

    updated unit-tests

--- src/test/java/com/geoffvargo/outco_practice/consecutive_subarray_sum/ConsecutiveSubarraySumTest.java
+++ src/test/java/com/geoffvargo/outco_practice/consecutive_subarray_sum/ConsecutiveSubarraySumTest.java
@@ -17,4 +17,27 @@ class ConsecutiveSubarraySumTest {
 		int[] arr    = {8, 3, 7, 9, 10, 1, 13};
 		assertEquals(false, ConsecutiveSubarraySum.consecSubarraySum(arr, target));
 	}
+	
+	@org.junit.jupiter.api.Test
+	void consecSubarraySum_TEST3() {
+		int   target = 33;
+		int[] arr    = {1, 4, 20, 3, 10, 5};
+		assertEquals(true, ConsecutiveSubarraySum.consecSubarraySum(arr, target));
+	}
+	
+	@org.junit.jupiter.api.Test
+	void consecSubarraySum_TEST4() {
+		int   target = 7;
+		int[] arr    = {1, 4, 0, 0, 3, 10, 5};
+		assertEquals(true, ConsecutiveSubarraySum.consecSubarraySum(arr, target));
+	}
+	
+	@org.junit.jupiter.api.Test
+	void consecSubarraySum_TEST5() {
+		int   target = 0;
+		int[] arr    = {1, 4};
+		boolean actual = ConsecutiveSubarraySum.consecSubarraySum(arr, target);
+		System.out.println(actual);
+		assertEquals(false, actual);
+	}
 }
